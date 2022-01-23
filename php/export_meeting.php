074614325f3c51140212ce56eba904385263d7a2
mmakos <misiek.makos@gmail.com>
2022-01-23 22:48:57 +0100

    Poprawiłem styl tytułu w eksportowaniu spotkania do html

--- php/export_meeting.php
+++ php/export_meeting.php
@@ -8,63 +8,66 @@
             $select_result = mysqli_query($connection, $sql_select);
             
             if ($select_result and $select_result->num_rows > 0) {
-                $html_file =  "<html><head>
-                                    <meta charset=\"UTF-8\">
-                                    <style>
-                                    #song h2 {
-                                    	color: black;
-                                    	font-weight: bold;
-                                    	font-family: arial;
-                                    	font-size: 133%;
-                                    }
-                                    
-                                    #song .author {
-                                    	line-height: 140%;
-                                    	font-size: 90%;
-                                    	margin-bottom: 20px;
-                                    	padding-left: 6px;
-                                    	font-family: verdana;
-                                    	border-left: 1px solid;
-                                    	margin-left: 12px
-                                    }
-                                    
-                                    #song span {
-                                    	color: black;
-                                    	font-family: verdana;
-                                    }
-                                    
-                                    #song sup {
-                                    	font-size: 0.6em;
-                                    }
-                                    
-                                    #song sub {
-                                    	font-size: 0.6em;
-                                    }
-                                    
-                                    #song td {
-                                    	padding-right: 20px;
-                                    	border: 0px;
-                                    	line-height: 1.25;
-                                    	white-space: nowrap;
-                                    	width: 1%;
-                                    }
-                                    
-                                    #song td:last-child {
-                                    	width: 100%
-                                    }
-                                    
-                                    #song table {
-                                    	border: 0px;
-                                    	letter-spacing: 0;
-                                    }
-                                    </style>
-                                </head><body>";
+                $html_file = "<html><head>
+    <meta charset=\"UTF-8\">
+    <style>
+    #song h2 {
+        color: black;
+        font-weight: bold;
+        font-family: arial;
+        font-size: 133%;
+    }
+
+    #song .author {
+        line-height: 140%;
+        font-size: 90%;
+        margin-bottom: 20px;
+        padding-left: 6px;
+        font-family: verdana;
+        border-left: 1px solid;
+        margin-left: 12px
+    }
+
+    #song span {
+        color: black;
+        font-family: verdana;
+    }
+
+    #song sup {
+        font-size: 0.6em;
+    }
+
+    #song sub {
+        font-size: 0.6em;
+    }
+
+    #song td {
+        padding-right: 20px;
+        border: 0px;
+        line-height: 1.25;
+        white-space: nowrap;
+        width: 1%;
+    }
+
+    #song td:last-child {
+        width: 100%
+    }
+
+    #song table {
+        border: 0px;
+        letter-spacing: 0;
+    }
+    </style>
+</head><body>";
                 
                 while($row = mysqli_fetch_array($select_result)) {
                     $content = $row['post_content'];
                     $title = $row['post_title'];
-                    
-                    $html_file = $html_file . "<h2>$title</h2>$content";
+
+                    $split_content = explode('>', $content, 2);
+                    if (count($split_content) > 1) {
+                        $html_file = $html_file . $split_content[0] . "><h2>$title</h2>" . $split_content[1];
+                    }
                 }
                 $html_file = $html_file . "</body></html>";
                 
